70c53a4efce3dcc8f7f0eb63c023a8e6b3ead7c0
Alberto Miranda <codealchemist@gmail.com>
2018-03-03 03:33:49 -0300

    Added reset endpoint; using get requests.

--- src/index.js
+++ src/index.js
@@ -12,18 +12,28 @@ const port = process.env.PORT || 8080
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-app.post('/log/:sensorId', (req, res) => {
+app.get('/log/:sensorId/:value', (req, res) => {
   const id = req.params.sensorId
-  const data = req.body
-  console.log(`SENSOR ${id}:`, data)
-  const item = {id, value: data.value}
+  const value = req.params.value
+  console.log(`SENSOR ${id}:`, value)
+  const item = {id, value}
   dbLogger.add(item, () => {
-    console.log(`saved ${id}:`, data)
+    console.log(`saved ${id}:`, value)
     res
       .status(200)
       .send(item)
   })
 })
+
+app.get('/reset/:sensorId/', (req, res) => {
+  const id = req.params.sensorId
+  dbLogger.reset(id, () => {
+    console.log(`reset ${id}`)
+    res
+      .status(200)
+      .send(id)
+  });
+});
 
 app.listen(port, function() {
   console.log(`listening at http://localhost:${port}`)
